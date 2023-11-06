61bd0cbecca942f50894182e83c1d44845000a2b
Yandra Karine <yandrakarine.dev@outlook.com>
2023-11-06 14:37:06 -0300

    A2.1: add Typescript

--- core/crud.ts
+++ core/crud.ts
@@ -1,13 +1,13 @@
-const fs = require("fs");
+import fs from "fs";
 const DB_FILE_PATH = "./core/db";
 
 console.log("[CRUD]");
 
-function create(content) {
+function create(content: string) {
   fs.writeFileSync(DB_FILE_PATH, content);
   return content;
 }
 
 // SIMULATION
 
-console.log(create("bom dia!!"));
+console.log(create("bom dia!!!"));
