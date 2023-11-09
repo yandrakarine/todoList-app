75bca8bc11e92feffeed5dd860f36987113aab9b
Yandra Karine <yandrakarine.dev@outlook.com>
2023-11-09 10:48:19 -0300

    A2.2: READ - Estruturas de dados para guardar informações

--- core/crud.ts
+++ core/crud.ts
@@ -3,11 +3,50 @@ const DB_FILE_PATH = "./core/db";
 
 console.log("[CRUD]");
 
+interface Todo {
+  date: string;
+  content: string;
+  done: boolean;
+}
+
 function create(content: string) {
-  fs.writeFileSync(DB_FILE_PATH, content);
+  const todo: Todo = {
+    date: new Date().toISOString(),
+    content: content,
+    done: false,
+  };
+
+  const todos = [...read(), todo];
+
+  fs.writeFileSync(
+    DB_FILE_PATH,
+    JSON.stringify(
+      {
+        todos,
+      },
+      null,
+      4
+    )
+  );
   return content;
 }
 
-// SIMULATION
+function read(): Array<Todo> {
+  const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
+  const db = JSON.parse(dbString || "{}"); // fazendo o tratamento de erro caso o db esteja vazio
+  if (!db.todos) {
+    return [];
+  }
+  return db.todos;
+}
 
-console.log(create("bom dia!!!"));
+function clearDB() {
+  fs.writeFileSync(DB_FILE_PATH, "");
+}
+
+// SIMULATION
+clearDB();
+create("bom dia!!");
+create("boa tarde");
+create("boa noite");
+console.log(read());
